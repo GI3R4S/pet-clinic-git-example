de0206c6d44f0d3f284cb6b57a89a0ac385c56f0
Przemyslaw Gesieniec <Przemyslaw.Gesieniec@ttpsc.pl>
2019-03-11 20:27:27 +0100

    initiali project

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -38,21 +38,19 @@ class VetController {
 
     @GetMapping("/vets.html")
     public String showVetList(Map<String, Object> model) {
-        // Here we are returning an object of type 'Vets' rather than a collection of Vet
-        // objects so it is simpler for Object-Xml mapping
         Vets vets = new Vets();
         vets.getVetList().addAll(this.vets.findAll());
         model.put("vets", vets);
         return "vets/vetList";
     }
 
-    @GetMapping({ "/vets" })
-    public @ResponseBody Vets showResourcesVetList() {
-        // Here we are returning an object of type 'Vets' rather than a collection of Vet
-        // objects so it is simpler for JSon/Object mapping
-        Vets vets = new Vets();
-        vets.getVetList().addAll(this.vets.findAll());
-        return vets;
-    }
+//    @GetMapping({ "/vets" })
+//    public @ResponseBody Vets showResourcesVetList() {
+//        // Here we are returning an object of type 'Vets' rather than a collection of Vet
+//        // objects so it is simpler for JSon/Object mapping
+//        Vets vets = new Vets();
+//        vets.getVetList().addAll(this.vets.findAll());
+//        return vets;
+//    }
 
 }
